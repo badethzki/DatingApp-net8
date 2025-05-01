db46e6aab0db8aaf4f9b4147ca67608616f8fbf5
badethzki <dev.bma.tengco@gmail.com>
2025-05-01 12:00:16 +0800

    End of Section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -9,10 +9,11 @@ public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
-        _ = CreateMap<AppUser, MemberDto>()
-            .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
-            .ForMember(d => d.PhotoUrl,
-                o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+        CreateMap<AppUser, MemberDto>()
+           .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+           .ForMember(d => d.PhotoUrl,
+               o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
         CreateMap<Photo, PhotoDto>();
+        CreateMap<MemberUpdateDto, AppUser>();
     }
 }
